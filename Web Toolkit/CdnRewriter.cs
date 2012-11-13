19d313c4d6843171976cc325e61903e8349df641
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2012-11-13 16:28:03 -0600

    Basic CDN domain replacement code.
    Uses regex, very picky about what is replaced. Need to upgrade to an
    HTML parser at some point in the future.

---
+++ Web Toolkit/CdnRewriter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NeoSmart.Web
+{
+    [Flags]
+    public enum RewriteObjects
+    {
+        None = 0,
+        Images = 0x01,
+        Css = 0x02,
+        JavaScript = 0x04,
+        All = 0xFF
+    }
+
+    public class CdnRewriteFilter : MemoryStream
+    {
+        private Stream _stream;
+        public string CdnDomain { get; set; }
+
+        public CdnRewriteFilter(Stream stream)
+        {
+            _stream = stream;
+        }
+
+        public RewriteObjects RewriteType { get; set; }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+
+            var html = Encoding.Default.GetString(buffer, offset, count);
+
+            if ((RewriteType & RewriteObjects.Images) == RewriteObjects.Images)
+            {
+                html = Regex.Replace(html, "<[^>]*img[^>]+src=\"(/[^/][^\"]+)\"", PrefixDomain, RegexOptions.IgnoreCase);
+                html = Regex.Replace(html, "<[^>]*img[^>]+src='(/[^/][^']+)'", PrefixDomain, RegexOptions.IgnoreCase);
+            }
+
+            if ((RewriteType & RewriteObjects.JavaScript) == RewriteObjects.JavaScript)
+            {
+                html = Regex.Replace(html, "\"(/[^/][^\"]+.js)\"", PrefixDomain, RegexOptions.IgnoreCase);
+                html = Regex.Replace(html, "'(/[^/][^\"]+.js)'", PrefixDomain, RegexOptions.IgnoreCase);
+            }
+
+            if ((RewriteType & RewriteObjects.Css) == RewriteObjects.Css)
+            {
+                html = Regex.Replace(html, "\"(/[^/][^\"]+.css)\"", PrefixDomain, RegexOptions.IgnoreCase);
+                html = Regex.Replace(html, "'(/[^/][^\"]+.css)'", PrefixDomain, RegexOptions.IgnoreCase);
+            }
+            
+            byte[] outData = Encoding.Default.GetBytes(html);
+            _stream.Write(outData, 0, outData.GetLength(0));
+        }
+
+        private string PrefixDomain(Match match)
+        {
+            return match.Value.Replace(match.Groups[1].Value, string.Format("//{0}{1}", CdnDomain, match.Groups[1].Value));
+        }
+    }
+}
