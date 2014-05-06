26799c338d540f8d1f28e713b2919aac3be3547d
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2014-05-06 00:04:42 -0500

    Added DictionaryExtensions class
    Contains Dictionary extension method SafeLookup which provides an option of
    specifying what is returned when a lookup fails, like TryGetValue but with
    an explicit alternative (vs TValue default).

---
+++ Web Toolkit/DictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoSmart.Web
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue SafeLookup<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue ifNotFound)
+        {
+            TValue value;
+            return dictionary.TryGetValue(key, out value) ? value : ifNotFound;
+        }
+    }
+}
