b1e92aee337fd4f2b53b3bf3be3df58a3bb373b3
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2014-11-30 12:36:12 -0600

    Forced to use non-generic, collection-specific code to implement TryGetValue and SafeLookup

--- Web Toolkit/NameObjectCollectionExtensions.cs
+++ Web Toolkit/NameObjectCollectionExtensions.cs
@@ -8,48 +8,24 @@ using System.Web;
 
 namespace NeoSmart.Web
 {
-    internal abstract class NameObjectCollection : NameObjectCollectionBase
-    {
-        public object GetAtIndex(int index)
-        {
-            return BaseGet(index);
-        }
-    }
-
-    public static class NameObjectColletionBaseExtensions
-    {
-        public static bool TryGetValue<T>(this NameObjectCollectionBase dictionary, string key, out T value)
-        {
-            for (int i = 0; i < dictionary.Count; ++i)
-            {
-                if (dictionary.Keys[i] == key)
-                {
-                    value = (T)((NameObjectCollection)dictionary).GetAtIndex(i);
-                    return true;
-                }
-            }
-
-            value = default(T);
-            return false;
-        }
-
-        public static T SafeLookup<T>(this NameObjectCollectionBase dictionary, string key, T ifNotFound = default(T))
-        {
-            T value;
-            return dictionary.TryGetValue(key, out value) ? value : ifNotFound;
-        }
-    }
-
     public static class NameValueCollectionExtensions
     {
         public static bool TryGetValue(this NameValueCollection dictionary, string key, out string value)
         {
-            return dictionary.TryGetValue<string>(key, out value);
+            if (dictionary.AllKeys.Contains(key))
+            {
+                value = dictionary.Get(key);
+                return true;
+            }
+
+            value = null;
+            return false;
         }
 
         public static string SafeLookup(this NameValueCollection dictionary, string key, string ifNotFound = "")
         {
-            return dictionary.SafeLookup<string>(key, ifNotFound);
+            string value;
+            return dictionary.TryGetValue(key, out value) ? value : ifNotFound;
         }
     }
 
@@ -57,12 +33,20 @@ namespace NeoSmart.Web
     {
         public static bool TryGetValue(this HttpCookieCollection dictionary, string key, out HttpCookie value)
         {
-            return dictionary.TryGetValue<HttpCookie>(key, out value);
+            if (dictionary.AllKeys.Contains(key))
+            {
+                value = dictionary.Get(key);
+                return true;
+            }
+
+            value = null;
+            return false;
         }
 
         public static HttpCookie SafeLookup(this HttpCookieCollection dictionary, string key, HttpCookie ifNotFound = default(HttpCookie))
         {
-            return dictionary.SafeLookup<HttpCookie>(key, ifNotFound);
+            HttpCookie value;
+            return dictionary.TryGetValue(key, out value) ? value : ifNotFound;
         }
     }
 }
