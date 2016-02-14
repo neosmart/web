eaf6dc67352a53b46da6e923369e70321d70853c
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2016-02-14 17:27:50 -0600

    Added response caching helper class EasyCache

---
+++ Web Toolkit/EasyCache.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+
+namespace NeoSmart.Web
+{
+    static public class EasyCache
+    {
+        static public void Cache(this HttpResponseBase response, TimeSpan expiresIn)
+        {
+            response.Cache.SetOmitVaryStar(true);
+            response.Cache.SetExpires(DateTime.UtcNow + expiresIn);
+            response.Cache.SetCacheability(HttpCacheability.Public);
+        }
+
+        public static void NoCache(this HttpResponseBase response)
+        {
+            response.Cache.SetOmitVaryStar(true);
+            response.Cache.SetExpires(DateTime.MinValue);
+            response.Cache.SetCacheability(HttpCacheability.NoCache);
+        }
+    }
+}
