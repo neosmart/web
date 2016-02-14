898400adfae5e3154ea93a6059598328d3598d92
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2016-02-14 17:31:45 -0600

    Extended SeoRedirect() with query string handling
    Now includes parameters to determine behavior and handling of query strings
    It remains backwards compatible, but the extra parameters add the ability to
    strip all query strings (default), preserve all query strings, or preserve
    only select query strings.
    
    To-do: Add automatic detection of function parameters via reflection and
    automatically whitelist them.

--- Web Toolkit/Seo.cs
+++ Web Toolkit/Seo.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Web;
@@ -21,30 +18,41 @@ namespace NeoSmart.Web
     public class Seo
     {
         private static readonly ConcurrentDictionary<string, CachedMethod> MethodCache = new ConcurrentDictionary<string, CachedMethod>();
-        public static void SeoRedirect(Controller controller, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
+
+        public static void SeoRedirect(Controller controller, string[] preserveQueryStrings, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
+        {
+            SeoRedirect(controller, false, preserveQueryStrings, filePath, lineNumber);
+        }
+
+        public static void SeoRedirect(Controller controller, bool preserveAllQueryStrings = false, [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
+        {
+            SeoRedirect(controller, preserveAllQueryStrings, null, filePath, lineNumber);
+        }
+
+        private static void SeoRedirect(Controller controller, bool preserveAllQueryStrings, string[] preservedQueryStrings, string filePath, int lineNumber)
         {
             string key = string.Format("{0}:{1}", filePath, lineNumber);
             CachedMethod lastMethod;
             if (!MethodCache.TryGetValue(key, out lastMethod))
             {
-                var method = new StackFrame(1).GetMethod();
+                var method = new StackFrame(2).GetMethod();
                 lastMethod = new CachedMethod
-                    {
-                        Action = method.Name,
-                        Controller = method.DeclaringType.Name.Remove(method.DeclaringType.Name.Length - "Controller".Length),
-                        IsIndex =  method.Name == "Index"
-                    };
+                {
+                    Action = method.Name,
+                    Controller = method.DeclaringType.Name.Remove(method.DeclaringType.Name.Length - "Controller".Length),
+                    IsIndex = method.Name == "Index"
+                };
                 MethodCache.TryAdd(key, lastMethod);
             }
 
             string destination;
-            if (DetermineSeoRedirect(controller, lastMethod, out destination))
+            if (DetermineSeoRedirect(controller, lastMethod, preserveAllQueryStrings, preservedQueryStrings, out destination))
             {
                 controller.Response.RedirectPermanent(destination);
             }
         }
 
-        private static bool DetermineSeoRedirect(Controller controller, CachedMethod method, out string destination)
+        private static bool DetermineSeoRedirect(Controller controller, CachedMethod method, bool preserveAllQueryStrings, string[] preservedQueryStrings, out string destination)
         {
             bool redirect = false;
             string currentAction = (string)controller.RouteData.Values["action"];
@@ -71,7 +79,7 @@ namespace NeoSmart.Web
             if ((method.IsIndex != destination.EndsWith("/")))
             {
                 redirect = true;
-                destination = string.Format("{0}{1}", destination.TrimEnd(new[] { '/' }), method.IsIndex ? "/" : "");
+                destination = string.Format("{0}{1}", destination.TrimEnd('/'), method.IsIndex ? "/" : "");
             }
 
             //No Index in link
@@ -80,6 +88,34 @@ namespace NeoSmart.Web
                 redirect = true;
                 const string search = "Index/";
                 destination = destination.Remove(destination.Length - search.Length);
+            }
+
+            //Query strings
+            if (!preserveAllQueryStrings)
+            {
+                if (HttpContext.Current.Request.QueryString.AllKeys.Any(
+                        queryString => !preservedQueryStrings.Contains(queryString)))
+                {
+                    redirect = true;
+
+                    var i = 0;
+                    StringBuilder qsBuilder = null;
+                    foreach (var key in HttpContext.Current.Request.QueryString.AllKeys.Where(key => preservedQueryStrings.Contains(key)))
+                    {
+                        var value = HttpContext.Current.Request.QueryString[key];
+                        qsBuilder = i == 0 ? new StringBuilder() : qsBuilder;
+                        qsBuilder.AppendFormat("{0}{1}{2}{3}", i == 0 ? '?' : '&', key,
+                            string.IsNullOrEmpty(value) ? "" : "=",
+                            string.IsNullOrEmpty(value) ? "" : HttpUtility.UrlEncode(value));
+
+                        ++i;
+                    }
+
+                    if (qsBuilder != null)
+                    {
+                        destination += qsBuilder.ToString();
+                    }
+                }
             }
 
             return redirect;
