3d963cb1e2ad980ce2be58bb47a202ed481c7d2a
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2016-12-14 07:48:48 -0600

    Moved extensions to ExtensionMethods project
    ...except for System.Web extensions, because I don't watn to add
    a depenedency to System.Web in ExtensionMethods at this time.

--- Web Toolkit/HttpCookieCollectionExtensions.cs
+++ Web Toolkit/HttpCookieCollectionExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,26 +7,6 @@ using System.Web;
 
 namespace NeoSmart.Web
 {
-    public static class NameValueCollectionExtensions
-    {
-        public static bool TryGetValue(this NameValueCollection dictionary, string key, out string value)
-        {
-            if (dictionary.AllKeys.Contains(key))
-            {
-                value = dictionary.Get(key);
-                return true;
-            }
-
-            value = null;
-            return false;
-        }
-
-        public static string SafeLookup(this NameValueCollection dictionary, string key, string ifNotFound = "")
-        {
-            string value;
-            return dictionary.TryGetValue(key, out value) ? value : ifNotFound;
-        }
-    }
 
     public static class HttpCookieCollectionExtensions
     {
