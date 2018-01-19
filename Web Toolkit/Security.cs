2c264f9468a797fe4b069009eff37164fe48d147
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-01-19 15:21:38 -0600

    Added Security class with ValidateReferer function
    Can be extended with more features in the future. Currently has a
    ValidateReferer function that is also implemented as an extension method
    for HttpRequest.

---
+++ Web Toolkit/Security.cs
@@ -0,0 +1,49 @@
+﻿using NeoSmart.ExtensionMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace NeoSmart.Web
+{
+    public static class Security
+    {
+        public static readonly SortedSet<string> ValidReferers = new SortedSet<string>(StringComparer.InvariantCultureIgnoreCase)
+        {
+            null,
+            ""
+        };
+
+        public static bool ValidateReferer(string referer, bool nothrow = false)
+        {
+            if (!ValidReferers.Contains(referer))
+            {
+                if (nothrow)
+                {
+                    return false;
+                }
+                throw new InvalidRefererException(referer);
+            }
+
+            return true;
+        }
+
+        public static bool ValidateReferer(this HttpRequestBase request, bool nothrow = false)
+        {
+            return ValidateReferer(request.UrlReferrer?.Host, nothrow);
+        }
+    }
+
+    public class InvalidRefererException : HttpException
+    {
+        public readonly string Referer;
+
+        public InvalidRefererException(string referer)
+            : base(403, "Access to this resource is restricted!")
+        {
+            Referer = referer;
+        }
+    }
+}
